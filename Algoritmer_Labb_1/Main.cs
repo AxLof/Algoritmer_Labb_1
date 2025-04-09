4ac0242ec8020429d4cf35bbb9ac1ab1298016cc
AxLof <h23axlof@du.se>
2025-04-09 12:17:27 +0200

    nya execution times

--- Algoritmer_Labb_1/Main.cs
+++ Algoritmer_Labb_1/Main.cs
@@ -5,7 +5,7 @@ for (long i = 100; i < 100000000; i*=2)
     NumberCounter.TestFindDuplicates(5, i);
 }
 
-for (long i = 100; i < 100000; i*=2)
+for (long i = 100; i < 102500; i*=2)
 {
     CalculateMaxSum.TestMaxSumAlgorithmV1(i);
 }
